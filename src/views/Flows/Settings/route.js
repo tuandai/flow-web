4851d95b51081de9f74b9bbdfb03740dc64a5ecf
Yang Guo <gy@fir.im>
2018-06-04 18:52:13 +0800

    remove step editor from router

--- src/views/Flows/Settings/route.js
+++ src/views/Flows/Settings/route.js
@@ -7,19 +7,15 @@ import Build from './Build'
 import Envs from './Envs'
 import Yml from './Yml'
 import Info from './Info'
-import Editor from './Editor'
 
 export default function (path, store) {
   return <Route path={path} component={Container}>
-    <IndexRedirect to='editor' />
-    <Route path='editor' component={Editor}>
-      <IndexRedirect to='build' />
-      <Route component={YmlContainer}>
-        <Route path='info' text='info' component={Info} navbar />
-        <Route path='build' text='build' component={Build} navbar />
-        <Route path='envs' text='envs' component={Envs} navbar />
-        <Route path='edit' text='ymledit' component={Yml} navbar />
-      </Route>
+    <IndexRedirect to='info' />
+    <Route component={YmlContainer}>
+      <Route path='info' text='info' component={Info} navbar />
+      <Route path='build' text='build' component={Build} navbar />
+      <Route path='envs' text='envs' component={Envs} navbar />
+      <Route path='edit' text='ymledit' component={Yml} navbar />
     </Route>
   </Route>
 }
